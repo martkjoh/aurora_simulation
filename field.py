2b2a76cdc69e8b22cc6583bce9f4314be1e9ec69
Martin <martin.johnsrud@gmail.com>
2020-02-19 00:32:09 +0100

    centralized

--- field.py
+++ field.py
@@ -12,16 +12,16 @@ from mpl_toolkits.mplot3d import Axes3D
 # Size of the simulated area
 L = 1
 # Number of points in each dimension
-N = 10
-# (r[1,x,y,z],r[2,x,y,z],r[3,x,y,z]) er en vektor plasert i (x, y, z)
-x, y, z = np.mgrid[-L:L:N*1j, -L:L:N*1j, -L:L:N*1j]
-r = np.array((x, y, z))
+N = 20
+# (x[1,x,y,z],x[2,x,y,z],x[3,x,y,z]) er en vektor plasert i (x, y, z)
+x = np.array((np.mgrid[-L:L:N*1j, -L:L:N*1j, -L:L:N*1j]))
 # Direction of the dipole
 mx, my, mz = 0, 0, 1
+dims = (N, N, N)
 m = np.array((
-    np.ones_like(x) * mx,
-    np.ones_like(y) * my, 
-    np.ones_like(z) * mz))
+    np.ones(dims) * mx,
+    np.ones(dims) * my, 
+    np.ones(dims) * mz))
 
 # levi-cevita
 eijk = np.zeros((3, 3, 3))
@@ -37,21 +37,26 @@ def dot(r1, r2):
     return np.einsum("ixyz,ixyz->xyz", r1, r2) 
 
 def D(f):
-    return np.array([np.gradient(f, axis = i+1) for i in range(3)])
+    return np.array([np.gradient(f, axis = i+1) for i in range(3)]) * N / (2 / L)
 
 def curl(f):
     Df = D(f)
     return np.einsum("ijk,jkxyz->ixyz", eijk, Df)
 
 # Mathematical functions
-def A(r):
-    return 1/(4*pi) * cross(r, m) / dot(r, r)**(3 / 2)
+def A(x):
+    r = np.sqrt(dot(x, x))
+    return 1/(4*pi) * cross(x/r, m) / r**2
+
+def B(x):
+    r = np.sqrt(dot(x, x))
+    return 1/(4*pi) * (3*dot(m, x)*x / r**2 - m) / r**3 
 
 
 # Array manip
 # Mask everything not inside radius (r1, r2)
 def mask_radialy_vec(f, r1, r2):
-    rr = dot(r, r)
+    rr = dot(x, x)
     f = np.ma.array(f)
     f = np.ma.array([np.ma.masked_where(rr < r1, f[i]) for i in range(3)])
     f = np.ma.array([np.ma.masked_where(rr > r2, f[i]) for i in range(3)])
@@ -61,16 +66,16 @@ fig = plt.figure()
 ax = Axes3D(fig)
 ax.grid(False)
 
-Ar = A(r)
-Br = curl(Ar)
+Ax = A(x)
+Bx = curl(Ax)
+B0x = B(x)
+print((Bx - B0x) / B0x)
 
-r1 = 0.1
-r2 = 1
-Ar = mask_radialy_vec(Ar, r1, r2)
-Br = mask_radialy_vec(Br, r1, r2)    
-
-ax.quiver(*r, *Ar, length = 0.2, pivot = "middle")
-ax.quiver(*r, *Br, length = 0.2, color = "red", pivot = "middle")
-# ax.scatter(*r, r2, c = np.ndarray.flatten(rmask))
-
+r1 = 1
+r2 = 2
+Bx = mask_radialy_vec(Bx, r1, r2)    
+ax.quiver(*x, *(Bx - B0x), color = "red", pivot = "middle")
 plt.show()
+
+
+# ax.scatter(*x, r2, c = np.ndarray.flatten(rmask))
--- particles.py
+++ particles.py
@@ -4,13 +4,24 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-m = np.array([-0.02, 0.02, 1.2])
-m = m / np.sqrt(m @ m)
-dt = 0.001
-T = 2
+dt = 0.005
+T = 15
 N = int(T / dt)
 L = 2
 
+# The magnetic dipole moment
+m = np.array([-0.02, 0.02, 1.2])
+m = m / np.sqrt(m @ m)
+
+# Number of points in the discrete vec.field
+points = 10
+dims = [points] * 3
+m_field = np.array((
+    np.ones(dims) * m[0],
+    np.ones(dims) * m[1], 
+    np.ones(dims) * m[2]))
+
+x_field = np.array((np.mgrid[-L:L:points*1j, -L:L:points*1j, -L:L:points*1j]))
 
 def RK4(ys, f, i):
     k1 = f(ys[i]) * dt
@@ -20,43 +31,78 @@ def RK4(ys, f, i):
     delta = 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     ys[i + 1]  = ys[i] + delta
 
-
+# Gives the B-field at a point
 def B(x):
-    r = x @ x
-    return (1/ 4 * pi) * (3*(m.T @ x) * x / r**2 - m) / r**3
+    r = np.sqrt(x @ x)
+    return (1/ 4 * pi) * (3*(m @ x) * x / r**2 - m) / r**3
+
+
+def dot_fields(f1, f2):
+    return np.einsum("ixyz,ixyz->xyz", f1, f2)
+
+# Returns a 4d array where B_l(x_i, y_j, z_k) = B[l, i, j, k]
+def B_field(x):
+    r = np.sqrt(dot_fields(x, x))
+    xm = dot_fields(x, m_field)
+    return 1/(4*pi) * (3*xm*x / r**2 - m_field) / r**3
 
 def f(y):
     return np.array([y[1], np.cross(y[1], B(y[0]))])
 
+# Array manip
+# Mask everything not inside radius (r1, r2)
+def mask_field(f, r1, r2):
+    r = np.sqrt(dot_fields(x_field, x_field))
+    f = np.ma.array(f)
+    f = np.ma.array([np.ma.masked_where(r < r1, f[i]) for i in range(3)])
+    f = np.ma.array([np.ma.masked_where(r > r2, f[i]) for i in range(3)])
+    return f
+
+def send_particles():
+    a = 4
+    b = 5
+    lines = []
+    for i in range(a):
+        for j in range(b):
+            # y[i, j, k] is the k'th coordinat of the j'th derivative of x wrt t at t = dt*i
+            ys = np.empty((N + 1, 2, 3))
+            # starting condititons
+            X0 = [-2, -1 + 0.5*j, 0.1 + 0.3*i]
+            XDot0 = [0.2, 0, 0]
+            y0 = np.array([X0, XDot0])
+
+            ys[0] = y0
+
+            for k in range(N):
+                RK4(ys, f, k)
+            
+            lines.append(ys)
+    return lines
+
+def plot_lines3D(ax, lines):
+    n = len(lines)
+    for i in range(n):
+        ax.pbaspect = [1.0, 1.0, 1.0]
+        ax.plot(*lines[i][:,0].T, color = cm.viridis(i/n))
+
+    origin = np.mgrid[0:1:1, 0:1:1, 0:1:1]
+    plt.quiver(*origin, *m, pivot = "middle", color = "red")
+    ax.scatter(*origin, s = 5000)
+
+# Plot a B field between radius r[0] and r[1]
+def plot_Bfield(ax, r):
+    Bx = B_field(x_field)
+    Bx = mask_field(Bx, *r)    
+    ax.quiver(*x_field, *Bx, pivot = "middle", length = 2)
+
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.axis("off")
 ax.pbaspect = [1.0, 1.0, 1.0]
-a = 4
-b = 5
-for i in range(a):
-    for j in range(b):
-        # y[i, j, k] is the k'th coordinat of the j'th derivative of x wrt t at t = dt*i
-        ys = np.empty((N + 1, 2, 3))
+ax.axis("off")
 
-        X0 = [-1, -0.2 + 0.1*j, 0.3 + 0.1*i]
-        XDot0 = [1, 0, 0]
-        y0 = np.array([X0, XDot0]) * L
+lines = send_particles()
+plot_lines3D(ax, lines)
+plot_Bfield(ax, (0.6, 2))
 
-        ys[0] = y0
-
-        for k in range(N):
-            RK4(ys, f, k)
-
-        ax.plot(*ys[:,0].T, color = cm.viridis((i*b + j)/(a*b)))
-
-
-A = np.mgrid[0:1:1, 0:1:1, 0:1:1]
-plt.quiver(*A, *m, pivot = "middle")
 plt.show()
-
-# class Particle:
-#     def __init__(self, x, xDot):
-#         self.x = x
-#         self.xDot = xDot
